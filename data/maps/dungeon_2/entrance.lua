9e6fce955d0e6b9e3e5549091577c83b51489058
Christopho <christopho@solarus-games.org>
2017-02-21 21:46:07 +0100

    Dungeon 2 parallax entrance

--- data/maps/dungeon_2/entrance.lua
+++ data/maps/dungeon_2/entrance.lua
@@ -10,26 +10,52 @@
 local map = ...
 local game = map:get_game()
 
-local _, map_height = map:get_size()
-local entrance_initial_x, entrance_initial_y = entrance:get_position()
+local parallax_config = {
+
+  {
+    entity = entrance,
+    ratio_y = -6,
+    reference_y = 189,
+  },
+
+  {
+    entity = building,
+    ratio_y = -12,
+    reference_y = 189,
+  },
+}
 
 -- Event called at initialization time, as soon as this map becomes is loaded.
 function map:on_started()
 
   game:set_hud_enabled(false)
 
-  entrance:remove_sprite()
+  for _, config in ipairs(parallax_config) do
+    config.entity_initial_x, config.entity_initial_y = config.entity:get_position()
+  end
+
+  sol.timer.start(map, 10, function()
+
+    local hero_x, hero_y = hero:get_position()
+
+    for _, config in ipairs(parallax_config) do
+      local entity = config.entity
+      local reference_x = config.reference_x or hero_x
+      local reference_y = config.reference_y or hero_y
+      local ratio_x = config.ratio_x or 1
+      local ratio_y = config.ratio_y or 1
+      local distance_x, distance_y = reference_x - hero_x, reference_y - hero_y
+      
+      local x = config.entity_initial_x + distance_x / ratio_x
+      local y = config.entity_initial_y + distance_y / ratio_y
+      entity:set_position(x, y)
+    end
+
+    return true
+  end)
 end
 
 function map:on_finished()
 
   game:set_hud_enabled(true)
-end
-
-function entrance:on_update()
-
-  local _, hero_y = hero:get_position()
-  local distance_y = map_height - hero_y
-  local entrance_y = entrance_initial_y - distance_y / 6
-  entrance:set_position(entrance_initial_x, entrance_y)
 end

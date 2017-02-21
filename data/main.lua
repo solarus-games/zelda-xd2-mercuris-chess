b8b2817497bb083d0d07fb7e6191a30a5524559c
Christopho <christopho@solarus-games.org>
2017-02-21 12:47:33 +0100

    Add Solarus team logo
    Closes #2.

--- data/main.lua
+++ data/main.lua
@@ -4,6 +4,18 @@
 
 require("scripts/features")
 local game_manager = require("scripts/game_manager")
+local solarus_logo = require("scripts/menus/solarus_logo")
+local team_logo = require("scripts/menus/team_logo")
+
+-- TODO
+-- local language_menu = require("scripts/menus/language")
+-- local title_screen = require("scripts/menus/title_screen")
+-- local savegames_menu = require("scripts/menus/savegames")
+
+local pre_game_menus = {
+  solarus_logo,
+  team_logo,
+}
 
 -- This function is called when Solarus starts.
 function sol.main:on_started()
@@ -11,14 +23,19 @@ function sol.main:on_started()
   -- Setting a language is useful to display text and dialogs.
   sol.language.set_language("fr")
 
-  local solarus_logo = require("scripts/menus/solarus_logo")
-
-  -- Show the Solarus logo initially.
-  sol.menu.start(self, solarus_logo)
-
-  -- Start the game when the Solarus logo menu is finished.
-  solarus_logo.on_finished = function()
-    game_manager:start_game("save1.dat")
+  -- Show the initial menus.
+  sol.menu.start(self, pre_game_menus[1])
+  for i, menu in ipairs(pre_game_menus) do
+    function menu:on_finished()
+      if sol.main.game ~= nil then
+        -- A game is already running (probably quick start with a debug key).
+        return
+      end
+      local next_menu = pre_game_menus[i + 1]
+      if next_menu ~= nil then
+        sol.menu.start(sol.main, next_menu)
+      end
+    end
   end
 
 end
@@ -47,4 +64,16 @@ function sol.main:on_key_pressed(key, modifiers)
   end
 
   return handled
+end
+
+-- Starts a game.
+function sol.main:start_savegame(game)
+
+  -- Skip initial menus if any.
+  for _, menu in ipairs(pre_game_menus) do
+    sol.menu.stop(menu)
+  end
+
+  sol.main.game = game
+  game:start()
 end

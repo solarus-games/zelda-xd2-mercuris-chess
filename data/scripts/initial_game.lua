933be95675d5c1c28578547de02425863e40f83a
Christopho <christopho@solarus-games.org>
2017-02-24 13:58:28 +0100

    Give ZSXD items initially

--- data/scripts/initial_game.lua
+++ data/scripts/initial_game.lua
@@ -16,13 +16,34 @@ function initial_game:initialize_new_savegame(game)
   -- and the starting location.
   game:set_starting_location("dungeon_2/entrance", nil)
 
+  game:set_ability("jump_over_water", 1)
+
   game:set_max_life(4 * 9)
   game:set_life(game:get_max_life())
+  game:get_item("rupee_bag"):set_variant(2)
+  game:set_money(0)
+
   game:get_item("tunic"):set_variant(1)
   game:get_item("sword"):set_variant(2)
   game:get_item("shield"):set_variant(1)
-  game:get_item("rupee_bag"):set_variant(2)
-  game:set_ability("jump_over_water", 1)
+
+  game:get_item("glove"):set_variant(1)
+  game:get_item("feather"):set_variant(1)
+  game:get_item("pegasus_shoes"):set_variant(1)
+
+  game:get_item("quiver"):set_variant(1)
+  local bow = game:get_item("bow")
+  bow:set_variant(1)
+  bow:set_amount(0)
+
+  game:get_item("bomb_bag"):set_variant(1)
+  local bombs_counter = game:get_item("bombs_counter")
+  bombs_counter:set_variant(1)
+  bombs_counter:set_amount(0)
+
+  game:get_item("bottle_1"):set_variant(1)
+  game:get_item("bottle_2"):set_variant(1)
+  game:get_item("bottle_3"):set_variant(1)
 end
 
 return initial_game

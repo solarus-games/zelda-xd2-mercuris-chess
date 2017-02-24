88956a4fd0561bc15e5edb9b51341a81fade72f0
Christopho <christopho@solarus-games.org>
2017-02-24 13:09:22 +0100

    Add more item scripts

--- data/scripts/initial_game.lua
+++ data/scripts/initial_game.lua
@@ -19,8 +19,7 @@ function initial_game:initialize_new_savegame(game)
   game:set_max_life(4 * 9)
   game:set_life(game:get_max_life())
   game:set_max_money(100)
-  game:set_ability("lift", 1)
-  game:set_ability("sword", 1)
+  game:set_ability("jump_over_water", 1)
 end
 
 return initial_game

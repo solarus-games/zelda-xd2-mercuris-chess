e9656fba05e766b4b888f0e3cd35a1c53d480593
Christopho <christopho@solarus-games.org>
2017-03-03 17:09:28 +0100

    Brandish the small key hidden between statues

---
+++ data/items/small_key_brandished.lua
@@ -0,0 +1,15 @@
+local item = ...
+
+function item:on_created()
+
+  self:set_shadow("small")
+  self:set_brandish_when_picked(true)
+  self:set_sound_when_picked(nil)
+  self:set_sound_when_brandished("picked_small_key")
+end
+
+function item:on_obtaining(variant, savegame_variable)
+
+  self:get_game():add_small_key()
+end
+

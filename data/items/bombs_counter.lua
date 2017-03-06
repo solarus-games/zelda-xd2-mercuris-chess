afa43c54b53ef29cf47ba39bcbbabbfbc44aa155
Christopho <christopho@solarus-games.org>
2017-03-06 21:48:58 +0100

    Add separator scripts

--- data/items/bombs_counter.lua
+++ data/items/bombs_counter.lua
@@ -1,28 +1,37 @@
 local item = ...
+local game = item:get_game()
+
+local sound_timer
 
 function item:on_created()
 
-  self:set_savegame_variable("possession_bombs_counter")
-  self:set_amount_savegame_variable("amount_bombs_counter")
-  self:set_assignable(true)
+  item:set_savegame_variable("possession_bombs_counter")
+  item:set_amount_savegame_variable("amount_bombs_counter")
+  item:set_assignable(true)
 end
 
 -- Called when the player uses the bombs of his inventory by pressing the corresponding item key.
 function item:on_using()
 
-  if self:get_amount() == 0 then
-    sol.audio.play_sound("wrong")
+  if item:get_amount() == 0 then
+    if sound_timer == nil then
+      sol.audio.play_sound("wrong")
+      sound_timer = sol.timer.start(game, 500, function()
+        sound_timer = nil
+      end)
+    end
   else
-    self:remove_amount(1)
-    local x, y, layer = self:create_bomb()
+    item:remove_amount(1)
+    local x, y, layer = item:create_bomb()
     sol.audio.play_sound("bomb")
   end
-  self:set_finished()
+  item:set_finished()
 end
 
 function item:create_bomb()
 
-  local hero = self:get_map():get_entity("hero")
+  local map = item:get_map()
+  local hero = map:get_entity("hero")
   local x, y, layer = hero:get_position()
   local direction = hero:get_direction()
   if direction == 0 then
@@ -35,10 +44,24 @@ function item:create_bomb()
     y = y + 16
   end
 
-  self:get_map():create_bomb{
+  local bomb = map:create_bomb{
     x = x,
     y = y,
     layer = layer
   }
+
+  map.current_bombs = map.current_bombs or {}
+  map.current_bombs[bomb] = true
 end
 
+function item:remove_bombs_on_map()
+
+  local map = item:get_map()
+  if map.current_bombs == nil then
+    return
+  end
+  for bomb in pairs(map.current_bombs) do
+    bomb:remove()
+  end
+  map.current_bombs = {}
+end
--- data/maps/dungeon_2/1f.lua
+++ data/maps/dungeon_2/1f.lua
@@ -1,6 +1,11 @@
 local map = ...
 local game = map:get_game()
 
+local separator_manager = require("maps/lib/separator_manager")
+separator_manager:manage_map(map)
+local door_manager = require("maps/lib/door_manager")
+door_manager:manage_map(map)
+
 function map:on_started()
 
   -- Access to the kitchen.

533417d55c2c1a7e5841126ce939debe7232fab0
Christopho <christopho@solarus-games.org>
2017-03-09 11:11:39 +0100

    Move map-related scripts

--- data/maps/dungeon_2/1f.lua
+++ data/maps/dungeon_2/1f.lua
@@ -1,9 +1,9 @@
 local map = ...
 local game = map:get_game()
 
-local separator_manager = require("maps/lib/separator_manager")
+local separator_manager = require("scripts/maps/separator_manager")
 separator_manager:manage_map(map)
-local door_manager = require("maps/lib/door_manager")
+local door_manager = require("scripts/maps/door_manager")
 door_manager:manage_map(map)
 
 local elevator_manager = require("scripts/maps/elevator_manager")

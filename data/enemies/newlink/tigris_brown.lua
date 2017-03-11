5ad10fd64aea2c1c32eeb2ed2e44eb6a5458791e
oclero <oclero@hotmail.com>
2017-03-11 17:19:23 +0100

    Introduction: cat fight

--- data/enemies/newlink/tigris_brown.lua
+++ data/enemies/newlink/tigris_brown.lua
@@ -4,7 +4,7 @@ local behavior = require("enemies/lib/towards_hero")
 
 local properties = {
   sprite = "enemies/" .. enemy:get_breed(),
-  life = 20,
+  life = 10,
   damage = 4,
   normal_speed = 48,
   faster_speed = 48
--- data/maps/main_village/link_cave.lua
+++ data/maps/main_village/link_cave.lua
@@ -13,12 +13,41 @@ local game = map:get_game()
 -- Event called at initialization time, as soon as this map becomes is loaded.
 function map:on_started()
 
-  -- You can initialize the movement and sprites of various
-  -- map entities here.
+  -- Open doors by default.
+  map:set_doors_open("cave_door_")
+  
+  tigriss_docile:set_visible(false)
 end
 
 -- Event called after the opening transition effect of the map,
 -- that is, when the player takes control of the hero.
 function map:on_opening_transition_finished()
 
+end
+
+-- Launch the boss fight.
+function door_sensor:on_activated()
+  -- Close doors.
+  map:close_doors("cave_door_")
+
+  -- 
+end
+
+-- When the boss is killed.
+function tigriss:on_dying()
+  local tigriss_x, tigriss_y = tigriss:get_position()
+  tigriss_docile:set_position(tigriss_x, tigriss_y)
+  tigriss_docile:set_visible(true)
+end
+
+-- Some sinisters sounds before fighting the boss...
+function miaou_sensor_1:on_activated()
+  local dialog_box = game:get_dialog_box()
+  game:start_dialog("chores.miaou_1")
+end
+
+-- Some sinisters sounds before fighting the boss...
+function miaou_sensor_2:on_activated()
+  local dialog_box = game:get_dialog_box()
+  game:start_dialog("chores.miaou_2")
 end

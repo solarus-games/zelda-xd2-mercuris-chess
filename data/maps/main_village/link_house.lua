1011a82bac641a3baf62a7d47e12962838447cc7
oclero <oclero@hotmail.com>
2017-03-11 12:24:12 +0100

    WIP: introduction (link's house)

--- data/maps/main_village/link_house.lua
+++ data/maps/main_village/link_house.lua
@@ -22,8 +22,10 @@ local camera_shaking_count = 0
 -- Event called at initialization time, as soon as this map becomes is loaded.
 function map:on_started(destination_point)
 
+  local intro_done = game:get_value("introduction_done")
+
   -- Game's introducton, normally called only one time
-  if destination_point ~= nil and destination_point:get_name() == "start_position" then
+  if intro_done == nil or not intro_done then
 
     -- Create white surface.
     local quest_w, quest_h = sol.video.get_quest_size()
@@ -32,21 +34,8 @@ function map:on_started(destination_point)
       white_surface:fill_color({255, 255, 255})
     end
 
-    -- Initialize cinematic black stripes.
-    should_draw_black_stripes = true
-    black_stripe = sol.surface.create(quest_w, 24)
-    black_stripe:fill_color({0, 0, 0})
-
-    -- Hide HUD.
-    game:set_hud_enabled(false)
-
-    -- Hide hero.
-    local hero = map:get_hero()
-    hero:freeze()
-    hero:set_visible(false)
-
-    -- Prevent the player from pausing the game
-    game:set_pause_allowed(false)
+    -- Set cinematic mode.
+    map:set_cinematic_mode(true)
 
     -- Instead, show Link in bed.
     local bed_hero_sprite = bed:get_sprite()
@@ -60,12 +49,13 @@ function map:on_started(destination_point)
 
     -- Start Zelda's dialog.
     sol.timer.start(map, 2000, function()
-
       local dialog_box = game:get_dialog_box()
       dialog_box:set_position("bottom")
       game:start_dialog("intro.zelda_waking_up", function()
         sol.timer.start(map, 2000, function()
           game:start_dialog("intro.zelda_shaking", function()
+            -- Shake the camera.
+            -- The next actions are done when camera shaking is finished.
             map:shake_camera()
           end)
         end)
@@ -75,11 +65,17 @@ function map:on_started(destination_point)
 
   -- Normal state
   else
+    zelda:set_position(104, 88, 1)
+    local zelda_sprite = zelda:get_sprite()
+    zelda_sprite:set_animation("stopped")
+    zelda_sprite:set_direction(3) -- down
+    
     snores:remove()
+    zelda_angry:remove()    
     game:set_hud_enabled(true)
-    sol.audio.play_music("village")
-    bed:set_position(56, 101)
-    zelda_angry:set_visible(false)    
+    sol.audio.play_music("alttp/village")
+    map:set_cinematic_mode(false)
+
   end
 end
 
@@ -189,20 +185,28 @@ function map:shake_camera()
               local bed_hero_sprite = bed:get_sprite()
               bed_hero_sprite:set_animation("hero_waking_aside")
               zelda_angry_sprite:set_paused(true)
-              zelda_angry:set_visible(false)
+              zelda_angry:remove()
               zelda:set_visible(true)
               
-              local zelda_sprite = zelda:get_sprite()
-              zelda_sprite:set_direction(0)
+              game:start_dialog("intro.zelda_waiting", function()
+                local zelda_movement = sol.movement.create("target")
+                local zelda_x, zelda_y = zelda:get_position()
+                zelda:set_position(zelda_x, zelda_y, 1)
+                zelda_movement:set_speed(30)
+                zelda_movement:set_smooth(true)
+                zelda_movement:set_ignore_obstacles(true)
+                zelda_movement:set_target(104, 88)
+                zelda_movement:start(zelda, function()
+                  zelda:stop_movement()
+                  local zelda_sprite = zelda:get_sprite()
+                  zelda_sprite:set_animation("stopped")
+                  zelda_sprite:set_direction(3) -- down
+                  sol.timer.start(map, 500, function()
+                    map:make_link_go_out_of_bed()
+                    game:set_value("introduction_done", true)
+                  end)
 
-              local zelda_x, zelda_y = zelda:get_position()
-              local zelda_movement = sol.movement.create("target")
-              zelda_movement:set_speed(30)
-              zelda_movement:set_smooth(true)
-              zelda_movement:set_target(zelda_x + 24, zelda_y)
-              zelda_movement:start(zelda, function()
-                -- tODO
-                print("TODO")
+                end)
               end)
 
             end)
@@ -216,8 +220,115 @@ end
 
 -- Link falls off his bed.
 function map:make_link_fall_off_bed()
-    local bed_hero_sprite = bed:get_sprite()
-    bed_hero_sprite:set_animation("hero_sleeping_aside")
-    local bed_hero_x, bed_hero_y = bed:get_position()
-    bed:set_position(bed_hero_x + 4, bed_hero_y)
+  local bed_hero_sprite = bed:get_sprite()
+  bed_hero_sprite:set_animation("hero_sleeping_aside")
+  local bed_hero_x, bed_hero_y = bed:get_position()
+  bed:set_position(bed_hero_x + 4, bed_hero_y)
+end
+
+-- Link finally wakes up and go out of his bed
+function map:make_link_go_out_of_bed()
+  -- Hide the hero in bed entity.
+  local bed_hero_sprite = bed:get_sprite()
+  bed_hero_sprite:set_animation("empty_open")
+  -- Show the real hero instead.
+  map:set_cinematic_mode(false)
+  hero:start_jumping(0, 8, true)
+  sol.audio.play_sound("hero_lands")
+end
+
+-- Enable or disable the cinematic mode
+function map:set_cinematic_mode(is_cinematic)
+  -- Initialize cinematic black stripes.
+  should_draw_black_stripes = is_cinematic
+  if should_draw_black_stripes then
+    if black_stripe == nil then
+      local quest_w, quest_h = sol.video.get_quest_size()
+      black_stripe = sol.surface.create(quest_w, 24)
+      black_stripe:fill_color({0, 0, 0})
+    end
+  else
+    black_stripe = nil
+  end
+
+  -- Hide or show HUD.
+  game:set_hud_enabled(not is_cinematic)
+
+  -- Hide of show hero.
+  local hero = map:get_hero()
+  if is_cinematic then
+    hero:freeze()
+    hero:set_visible(false)
+  else
+    hero:unfreeze()
+    hero:set_visible(true)
+  end
+  -- Prevent or allow the player from pausing the game
+  game:set_pause_allowed(not is_cinematic)
+
+  -- Track the hero with the camera.
+  if not is_cinematic then
+    map:get_camera():start_tracking(hero)
+  end
+end
+
+function zelda:on_interaction()
+
+  -- Get chores state.
+  local chore_step, chores_done = map:get_chores_state()
+
+  -- Step 0: Feed the cat.
+  if chore_step == 0 then
+    game:start_dialog("chores.chore_0", function()
+      -- Give the player the cat food if he has not got it yet.
+      -- game:has_item()
+    end)
+
+  -- Step 1: Cut the grass in the garden.
+  elseif chore_step == 1 then
+
+  -- Step 2: Do Zelda grocery (buy apple pie).
+  elseif chore_step == 2 then
+
+  end
+
+end
+
+-- Get the chores step.
+-- Returns a pair: (number) chore_step, (boolean) chores_done 
+function map:get_chores_state()
+  local chores_done = game:get_value("introduction_chores_done")
+  local chore_step = game:get_value("introduction_chore_step")
+  
+  if chores_done ==  nil then
+    chores_done = false
+  end
+
+  if chore_step == nil then
+    chore_step = 0
+  end
+  return chore_step, chores_done
+end
+
+-- Prevent the player from leaving
+function dont_leave_sensor:on_activated()
+
+  -- Get chores state.
+  local chore_step, chores_done = map:get_chores_state()
+
+  -- Link cant leave the house only if he has not done
+  -- the first chore at least one time.
+  if chore_step == 0 and not chores_done then
+    game:start_dialog("chores.dont_leave", function()
+      hero:freeze()
+      hero:set_animation("walking")
+      hero:set_direction(1)
+      local movement = sol.movement.create("path")
+      movement:set_path({ 2 })
+      movement:set_speed(88)
+      movement:start(hero, function()
+        hero:unfreeze()
+      end)
+    end)
+  end
 end

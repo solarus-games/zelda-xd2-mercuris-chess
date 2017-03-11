f39b9490b6b0022d27106f381579f029a7d1d64e
BenObiWan <benobiwan@gmail.com>
2017-03-11 23:34:42 +0100

    Dialog for the "no entry" guy.

--- data/maps/out/a2.lua
+++ data/maps/out/a2.lua
@@ -6,3 +6,7 @@ function map:on_started()
   local movement = sol.movement.create("random_path")
   movement:start(the_doctor)
 end
+
+function no_entry_sensor:on_activated()
+  game:start_dialog("chill_valley.no_entry_2")
+end

9c5d64ce8ca57bb51b48eacd2468f02fd16175b1
Christopho <christopho@solarus-games.org>
2017-03-14 11:46:58 +0100

    Flying tiles

--- data/maps/dungeon_2/3f.lua
+++ data/maps/dungeon_2/3f.lua
@@ -9,3 +9,14 @@ door_manager:manage_map(map)
 local elevator_manager = require("scripts/maps/elevator_manager")
 elevator_manager:create_elevator(map, "elevator_a", 0, 3)
 elevator_manager:create_elevator(map, "elevator_b", 0, 8)
+
+local flying_tile_manager = require("scripts/maps/flying_tile_manager")
+flying_tile_manager:create_flying_tiles(map, "flying_tile")
+
+function map:on_started(destination)
+
+  if destination == from_4f_se then
+    -- Don't enable flying tiles when coming from this way.
+    flying_tile_sensor:set_enabled(false)
+  end
+end

989ecc5a00f17462faee6b270ecf71d595e59bb4
Christopho <christopho@solarus-games.org>
2017-03-19 16:13:34 +0100

    Make chess pieces give the same damage

--- data/enemies/alttp/pike_fixed.lua
+++ data/enemies/alttp/pike_fixed.lua
@@ -5,7 +5,7 @@ local enemy = ...
 function enemy:on_created()
 
   self:set_life(1)
-  self:set_damage(4)
+  self:set_damage(1)
   self:create_sprite("enemies/" .. enemy:get_breed())
   self:set_size(16, 16)
   self:set_origin(8, 13)

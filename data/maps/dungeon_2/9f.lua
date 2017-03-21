fd3b3115b560a87dca4d0764200c24e0bc9c3d9a
Christopho <christopho@solarus-games.org>
2017-03-21 11:11:53 +0100

    Start dungeon 2 boss

--- data/maps/dungeon_2/9f.lua
+++ data/maps/dungeon_2/9f.lua
@@ -9,7 +9,37 @@ door_manager:manage_map(map)
 local elevator_manager = require("scripts/maps/elevator_manager")
 elevator_manager:create_elevator(map, "elevator_b", 0, 8, "vip_card")
 
+local fighting_boss = false
+
 function map:on_started()
 
   map:set_doors_open("boss_door", true)
+end
+
+function start_boss_sensor:on_activated()
+
+  if game:get_value("dungeon_2_boss") then
+    return
+  end
+
+  if fighting_boss then
+    return
+  end
+
+  map:close_doors("boss_door")
+  sol.audio.stop_music()
+end
+
+function grump_npc:on_interaction()
+
+  game:start_dialog("dungeon_2.9f.boss_start", function()
+    hero:freeze()
+    sol.timer.start(map, 1000, function()
+      sol.audio.play_music("alttp/ganon_battle")
+      grump_npc:set_enabled(false)
+      hero:unfreeze()
+    end)
+    fighting_miniboss = true
+  end)
+
 end

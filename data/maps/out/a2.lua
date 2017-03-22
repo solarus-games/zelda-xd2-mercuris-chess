8425a96e110921c0158dc77875fe83f8b1c74974
Christopho <christopho@solarus-games.org>
2017-03-22 23:25:18 +0100

    Fix chill valley dialogs

--- data/maps/out/a2.lua
+++ data/maps/out/a2.lua
@@ -1,12 +1,6 @@
 local map = ...
 local game = map:get_game()
 
-function map:on_started()
-
-  local movement = sol.movement.create("random_path")
-  movement:start(the_doctor)
-end
-
 function no_entry_sensor:on_activated()
   game:start_dialog("chill_valley.no_entry_2")
 end

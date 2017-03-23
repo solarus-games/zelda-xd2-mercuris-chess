23423bcf72759934dcfc6cd5f574333d97b9f919
Christopho <christopho@solarus-games.org>
2017-03-23 11:24:38 +0100

    Add doc NPC

--- data/maps/out/a2.lua
+++ data/maps/out/a2.lua
@@ -1,6 +1,12 @@
 local map = ...
 local game = map:get_game()
 
+function map:on_started()
+
+  local movement = sol.movement.create("random_path")
+  movement:start(doc)
+end
+
 function no_entry_sensor:on_activated()
   game:start_dialog("chill_valley.no_entry_2")
 end

b9280b7125ddc1284e5b8ad47ebf9cda8da5478a
Thomas Noury <thomas.noury@is06.com>
2017-03-24 01:24:19 +0100

    More rooms finished

--- data/maps/dungeon_1/b2.lua
+++ data/maps/dungeon_1/b2.lua
@@ -10,15 +10,31 @@
 local map = ...
 local game = map:get_game()
 
--- Event called at initialization time, as soon as this map becomes is loaded.
 function map:on_started()
+  -- rotating platform states
+  local rp1_state = game:get_value("dungeon_1_b2_rp1_state")
+  local rp2_state = game:get_value("dungeon_1_b2_rp1_state")
+  map:get_entity("rp_1"):set_state(rp1_state)
+  map:get_entity("rp_2"):set_state(rp2_state)
 
-  -- You can initialize the movement and sprites of various
-  -- map entities here.
+  -- miniboss
+  map:set_doors_open("miniboss_door")
 end
 
--- Event called after the opening transition effect of the map,
--- that is, when the player takes control of the hero.
-function map:on_opening_transition_finished()
+function map:on_finished()
+  -- save rotating platform states
+  local rp1_state = map:get_entity("rp_1"):get_state()
+  local rp2_state = map:get_entity("rp_2"):get_state()
+  game:set_value("dungeon_1_b2_rp1_state", rp1_state)
+  game:set_value("dungeon_1_b2_rp2_state", rp2_state)
+end
 
+-- mini boss room
+function miniboss_sensor:on_activated()
+  map:close_doors("miniboss_door")
+end
+
+function miniboss_weak_wall:on_collision_explosion()
+  map:remove_entities("miniboss_weak_wall")
+  sol.audio.play_sound("secret")
 end

a34a9111e32743efab28fa7c89fec78bcc0ee7bb
Christopho <christopho@solarus-games.org>
2017-03-24 17:06:37 +0100

    Temporarily fix pegasus boots

--- data/items/pegasus_shoes.lua
+++ data/items/pegasus_shoes.lua
@@ -1,16 +1,27 @@
 local item = ...
+local game = ...
 
 function item:on_created()
 
   self:set_savegame_variable("possession_pegasus_shoes")
   self:set_assignable(true)
+
+  -- TODO only if shoelaces
+  game:set_ability("run", 1)
 end
 
 function item:on_using()
 
-  local hero = self:get_map():get_entity("hero")
-  hero:set_direction(math.random(0, 3))
+-- TODO
+--  local hero = self:get_map():get_entity("hero")
+--  hero:set_direction(math.random(0, 3))
   hero:start_running()
   self:set_finished()
+end
+
+function item:on_variant_changed(variant)
+
+  -- TODO only if shoelaces
+  game:set_ability("run", variant)
 end
 

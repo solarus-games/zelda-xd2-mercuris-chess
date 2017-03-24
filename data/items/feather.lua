4494356204f2be7e92a961625d03f4cf08940cc8
Christopho <christopho@solarus-games.org>
2017-03-24 17:03:49 +0100

    Temporarily fix the feather

--- data/items/feather.lua
+++ data/items/feather.lua
@@ -5,6 +5,7 @@ function item:on_created()
   self:set_assignable(true)
 end
 
+--[[ TODO make 2 variants
 function item:on_using()
 
   sol.audio.play_sound("jump")
@@ -23,4 +24,13 @@ function item:on_using()
       math.random(16, 100), false)
   self:set_finished()
 end
+--]]
 
+
+function item:on_using()
+  sol.audio.play_sound("jump")
+  local hero = self:get_map():get_entity("hero")
+  local direction4 = hero:get_direction()
+  hero:start_jumping(direction4 * 2, 32, false)
+  self:set_finished()
+end

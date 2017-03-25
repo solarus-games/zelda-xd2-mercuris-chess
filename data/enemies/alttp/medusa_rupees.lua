0f27139c5ca66478163b080404ab57bb97ab7f1f
Christopho <christopho@solarus-games.org>
2017-03-25 14:22:22 +0100

    Make Rupee Medusa only shoot rupees

--- data/enemies/alttp/medusa_rupees.lua
+++ data/enemies/alttp/medusa_rupees.lua
@@ -9,15 +9,11 @@ function enemy:get_projectile_breed_and_sound()
 
   local n = math.random(100)
 
-  if n <= 75 then
-    return "alttp/fireball_small_triple_red", "zora"
-  end
-
-  if n <= 90 then
+  if n <= 50 then
     return "rupee_green", "rupee_counter_end"
   end
 
-  if n <= 97 then
+  if n <= 80 then
     return "rupee_blue", "rupee_counter_end"
   end
 

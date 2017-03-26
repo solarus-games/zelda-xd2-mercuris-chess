35451cd3535f3218437429f7194fe2bb16b941cf
oclero <oclero@hotmail.com>
2017-03-26 18:36:24 +0200

    Sunset creek: map almost done 99%

--- data/maps/sunset_creek.lua
+++ data/maps/sunset_creek.lua
@@ -10,6 +10,16 @@
 local map = ...
 local game = map:get_game()
 
+-- Overlay surface for changing colors.
+local quest_w, quest_h = sol.video.get_quest_size()
+local overlay_surface_1 = sol.surface.create(quest_w, quest_h)
+overlay_surface_1:fill_color({112, 76, 0})
+overlay_surface_1:set_blend_mode("add")
+
+local overlay_surface_2 = sol.surface.create(quest_w, quest_h)
+overlay_surface_2:fill_color({255, 155, 197})
+overlay_surface_2:set_blend_mode("multiply")
+
 -- Event called at initialization time, as soon as this map becomes is loaded.
 function map:on_started()
 
@@ -21,4 +31,10 @@ end
 -- that is, when the player takes control of the hero.
 function map:on_opening_transition_finished()
 
+end
+
+-- Call when map needs to be drawn.
+function map:on_draw(dst_surface)
+   overlay_surface_2:draw(dst_surface)
+   overlay_surface_1:draw(dst_surface)
 end

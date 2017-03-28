bc12beade5d70ebad8d0741853bee863e7e24fe3
Christopho <christopho@solarus-games.org>
2017-03-28 16:30:27 +0200

    Lens of truth

--- data/items/lens_of_truth.lua
+++ data/items/lens_of_truth.lua
@@ -8,7 +8,7 @@ local lens_surface = sol.surface.create("hud/lens_of_truth.png")
 lens_surface:set_opacity(128)
 
 function lens_menu:on_draw(dst_surface)
-  dst_surface:draw(lens_surface)
+  lens_surface:draw(dst_surface)
 end
 
 function item:on_created()
@@ -28,9 +28,10 @@ end
 
 function item:on_using()
 
-  if sol.menu.is_started(lens_menu) then
+  if active then
     sol.audio.play_sound("lens_end")
     sol.menu.stop(lens_menu)
+    active = false
     item:set_finished()
     return
   end
@@ -42,9 +43,31 @@ function item:on_using()
   end
 
   sol.audio.play_sound("lens_start")
-  sol.menu.start(game, lens_menu, false)
+  active = true
+  local map = game:get_map()
+  sol.menu.start(map, lens_menu, false)
+  -- It should be a map menu to avoid ordering issues
+  -- with the pause menu or the dialog box.
 
-  -- TODO remove magic
+  game:remove_magic(1)
+  sol.timer.start(game, 1500, function()
+    game:remove_magic(1)
+    if game:get_magic() == 0 then
+      sol.audio.play_sound("lens_end")
+      sol.menu.stop(lens_menu)
+      active = false
+      return
+    end
+    return true
+  end)
 
   item:set_finished()
+end
+
+function item:on_map_changed(map)
+
+  -- Keep the lens of truth active accross maps.
+  if active then
+    sol.menu.start(map, lens_menu, false)
+  end
 end

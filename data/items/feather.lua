c529ec1106dec5f8795c79ff550547c7da8e956e
Christopho <christopho@solarus-games.org>
2017-03-28 20:54:53 +0200

    Add the iPlume

--- data/items/feather.lua
+++ data/items/feather.lua
@@ -5,32 +5,34 @@ function item:on_created()
   self:set_assignable(true)
 end
 
---[[ TODO make 2 variants
 function item:on_using()
 
-  sol.audio.play_sound("jump")
   local hero = self:get_map():get_entity("hero")
   local direction4 = hero:get_direction()
-  local randdirection = math.random(4)
-  local diagonal = 0
 
-  if randdirection == 1 then
-    diagonal = 1
-  elseif randdirection == 4 then
-    diagonal = -1
+  if item:get_variant() == 1 then
+    -- Broken feather.
+    sol.audio.play_sound("jump")
+    local rand_direction = math.random(4)
+    local diagonal = 0
+
+    if rand_direction == 1 then
+      diagonal = 1
+    elseif rand_direction == 4 then
+      diagonal = -1
+    end
+
+    hero:start_jumping((direction4 * 2 + diagonal) % 8,
+        math.random(16, 40), false)
+
+  else
+    -- iFeather.
+    sol.audio.play_sound("jump")
+    local hero = self:get_map():get_entity("hero")
+    local direction4 = hero:get_direction()
+    hero:start_jumping(direction4 * 2, 32, false)
+    self:set_finished()
   end
 
-  hero:start_jumping((direction4 * 2 + diagonal) % 8,
-      math.random(16, 40), false)
-  self:set_finished()
-end
---]]
-
-
-function item:on_using()
-  sol.audio.play_sound("jump")
-  local hero = self:get_map():get_entity("hero")
-  local direction4 = hero:get_direction()
-  hero:start_jumping(direction4 * 2, 32, false)
-  self:set_finished()
+  item:set_finished()
 end

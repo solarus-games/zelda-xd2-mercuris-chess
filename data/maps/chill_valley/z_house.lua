7943724d22cf27a105abced2eed62f15117e8ebc
Renkineko <renkineko+github@hotmail.fr>
2017-03-29 21:18:14 +0200

    Z House in Chill Valley: Library

--- data/maps/chill_valley/z_house.lua
+++ data/maps/chill_valley/z_house.lua
@@ -22,3 +22,19 @@ end
 function map:on_opening_transition_finished()
 
 end
+
+function encyclopedia:on_interaction()
+  if game:get_value('chill_valley_z_house_heart_container_obtained') ~= true then
+    game:start_dialog("chill_valley.z_house.encyclopedia.not_opened", function(answer)
+      if answer == 1 then
+        map:get_hero():start_treasure("heart_container", 1, "chill_valley_z_house_heart_container_obtained", function()
+          game:start_dialog("chill_valley.z_house.encyclopedia.when_opened")
+        end)
+      else
+        game:start_dialog("chill_valley.z_house.encyclopedia.back_on_shelf")
+      end
+    end)
+  else
+    game:start_dialog("chill_valley.z_house.encyclopedia.already_opened")
+  end
+end

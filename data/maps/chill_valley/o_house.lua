92bdc835c2de23f1eb0920a08cda668731fee120
Christopho <christopho@solarus-games.org>
2017-03-29 09:07:51 +0200

    Add doctor dialog

--- data/maps/chill_valley/o_house.lua
+++ data/maps/chill_valley/o_house.lua
@@ -1,8 +1,11 @@
 local map = ...
 local game = map:get_game()
 
-function map:on_started()
+local num_dialogs = 2
+local dialog_index = math.random(num_dialogs)
 
-  local movement = sol.movement.create("random_path")
-  movement:start(the_doctor)
+function random_walk_npc_doctor:on_interaction()
+
+  game:start_dialog("chill_valley.o_house.doctor_" .. dialog_index)
+  dialog_index = dialog_index % num_dialogs + 1
 end

36afa056293d5e6439b1b4eae43c5875fa86f642
Christopho <christopho@solarus-games.org>
2017-03-30 05:10:17 +0200

    Tunnels

--- data/maps/caves/tunnels.lua
+++ data/maps/caves/tunnels.lua
@@ -5,5 +5,9 @@ local separator_manager = require("scripts/maps/separator_manager")
 separator_manager:manage_map(map)
 
 function map:on_started()
+  map:set_light(0)
+end
 
+function weak_wall_a:on_opened()
+  sol.audio.play_sound("secret")
 end

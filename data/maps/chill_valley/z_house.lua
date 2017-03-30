8ed27f7ed69a843e7021d9601c3b2b21ad910cf5
Renkineko <renkineko+github@hotmail.fr>
2017-03-30 21:33:59 +0200

    Added rat enemy in chill valley z_house
    Also added an achievement for killing that rat.
    In english, the humor doesn't work: it's a bookworm and
    not a "rat de bibliothèque". Maybe we can adapt with a snake
    depending on the language, but it's not the priority. Or saying
    like in Jack Frost (the movie): it's the french branch. Don't know

--- data/maps/chill_valley/z_house.lua
+++ data/maps/chill_valley/z_house.lua
@@ -15,12 +15,62 @@ function map:on_started()
 
   -- You can initialize the movement and sprites of various
   -- map entities here.
+
+    
+  for npc in map:get_entities_by_type('npc') do
+    if npc:get_name() ~= "encyclopedia" then
+      function npc:on_interaction()
+        game:start_dialog("chill_valley.z_house." .. npc:get_name(), function()
+          game:set_value("chill_valley_z_house_" .. npc:get_name() .. "_read", true)
+          if game:get_value('chill_valley_z_house_library_rat') ~= true then
+            map:check_rat_appear()
+          end
+        end)
+      end
+    end
+  end
+  
+  if game:get_value('chill_valley_z_house_library_rat') ~= true then
+    function library_rat:on_dead()
+      sol.audio.play_sound("treasure")
+      sol.audio.play_music("alttp/village")
+      game:start_dialog("chill_valley.z_house.achievement")
+      map:get_hero():start_victory()
+    end
+  end
+
+  map:check_rat_appear()
 end
 
 -- Event called after the opening transition effect of the map,
 -- that is, when the player takes control of the hero.
 function map:on_opening_transition_finished()
 
+end
+
+function map:check_rat_appear()
+  if game:get_value('chill_valley_z_house_library_rat') ~= true then
+    local all_read = true
+    for npc in map:get_entities_by_type('npc') do
+      if npc:get_name() ~= "encyclopedia" then
+        all_read = all_read and game:get_value("chill_valley_z_house_" .. npc:get_name() .. "_read") == true
+      
+        if not all_read then
+          break
+        end
+      end
+    end
+    
+    library_rat:set_enabled(all_read)
+    if all_read then
+      sol.audio.play_music("alttp/soldiers")
+      if game:get_value("chill_valley_z_house_all_read") ~= true then
+        game:start_dialog("chill_valley.z_house.all_read", function()
+          game:set_value("chill_valley_z_house_all_read", true)
+        end)
+      end
+    end
+  end
 end
 
 function encyclopedia:on_interaction()

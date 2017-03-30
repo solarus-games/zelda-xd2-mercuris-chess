b17c6c261f36dad9349c50cf30ae028bd480c2fe
Christopho <christopho@solarus-games.org>
2017-03-30 03:03:44 +0200

    Freaking cave

--- data/maps/caves/freaking_cave.lua
+++ data/maps/caves/freaking_cave.lua
@@ -1,6 +1,9 @@
 local map = ...
 local game = map:get_game()
 
+local separator_manager = require("scripts/maps/separator_manager")
+separator_manager:manage_map(map)
+
 function map:on_started()
   map:set_light(0)
 end

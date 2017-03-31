f70988c3b83111403dd67b11b29d54de759a3acc
Christopho <christopho@solarus-games.org>
2017-03-31 20:32:38 +0200

    Fix indentation

--- data/maps/final_scene.lua
+++ data/maps/final_scene.lua
@@ -123,96 +123,97 @@ function map:start_cinematic()
               game:start_dialog("final.mr_grump_3", function()
                 sol.timer.start(map, 500, function()
                   game:start_dialog("final.zelda_6", player_name, function()
-                  
-                  -- Make the player disapear
-                  local hero_movement_2 = sol.movement.create("target")
-                  hero:set_direction(2) -- left
-                  hero:set_animation("walking")
-                  hero_movement_2:set_target(-32, 192)
-                  hero_movement_2:set_speed(80)
-                  hero_movement_2:set_smooth(true)
-                  hero_movement_2:set_ignore_obstacles(true)
-                  hero_movement_2:start(hero, function()
 
-                    sol.timer.start(map, 500, function()
-                      -- Then come back with cocktails
-                      local hero_movement_3 = sol.movement.create("target")
-                      hero:set_direction(0) -- right
-                      hero:set_animation("carrying_walking")
-                      hero_movement_3:set_target(104, 192)
-                      hero_movement_3:set_speed(80)
-                      hero_movement_3:set_smooth(true)
-                      hero_movement_3:set_ignore_obstacles(true)
+                    -- Make the player disappear
+                    local hero_movement_2 = sol.movement.create("target")
+                    hero:set_direction(2) -- left
+                    hero:set_animation("walking")
+                    hero_movement_2:set_target(-32, 192)
+                    hero_movement_2:set_speed(80)
+                    hero_movement_2:set_smooth(true)
+                    hero_movement_2:set_ignore_obstacles(true)
+                    hero_movement_2:start(hero, function()
 
-                      -- Make the cocktails move along the hero
-                      cocktails:get_sprite():set_animation("walking")
-                      local hero_x, hero_y = hero:get_position()
-                      cocktails:set_position(hero_x, hero_y - 16)
+                      sol.timer.start(map, 500, function()
+                        -- Then come back with cocktails
+                        local hero_movement_3 = sol.movement.create("target")
+                        hero:set_direction(0) -- right
+                        hero:set_animation("carrying_walking")
+                        hero_movement_3:set_target(104, 192)
+                        hero_movement_3:set_speed(80)
+                        hero_movement_3:set_smooth(true)
+                        hero_movement_3:set_ignore_obstacles(true)
 
-                      function hero_movement_3:on_position_changed()
-                        if cocktails then
-                          local hero_x, hero_y = hero:get_position()
-                          cocktails:set_position(hero_x, hero_y - 16)                      
+                        -- Make the cocktails move along the hero
+                        cocktails:get_sprite():set_animation("walking")
+                        local hero_x, hero_y = hero:get_position()
+                        cocktails:set_position(hero_x, hero_y - 16)
+
+                        function hero_movement_3:on_position_changed()
+                          if cocktails then
+                            local hero_x, hero_y = hero:get_position()
+                            cocktails:set_position(hero_x, hero_y - 16)                      
+                          end
                         end
-                      end
 
-                      hero_movement_3:start(hero, function()
+                        hero_movement_3:start(hero, function()
 
-                        hero:set_animation("carrying_stopped")
-                        cocktails:get_sprite():set_animation("on_ground")
-                        sol.timer.start(map, 1500, function()
-                        
-                          cocktails:remove()
-                          hero:set_animation("stopped")
+                          hero:set_animation("carrying_stopped")
+                          cocktails:get_sprite():set_animation("on_ground")
+                          sol.timer.start(map, 1500, function()
+                          
+                            cocktails:remove()
+                            hero:set_animation("stopped")
 
-                          game:start_dialog("final.zelda_7", player_name, function()
-                            sol.timer.start(map, 500, function()
-                              hero:set_animation("dying")
+                            game:start_dialog("final.zelda_7", player_name, function()
+                              sol.timer.start(map, 500, function()
+                                hero:set_animation("dying")
 
-                              sol.timer.start(map, 2000, function()
-                                local hero_movement_4 = sol.movement.create("target")
-                                hero:set_animation("walking")
-                                hero:set_direction(2) -- left
-                                hero_movement_4:set_target(-32, 192)
-                                hero_movement_4:set_speed(80)
-                                hero_movement_4:set_smooth(true)
-                                hero_movement_4:set_ignore_obstacles(true)
-                                hero_movement_4:start(hero, function()
-                                  sol.timer.start(map, 1000, function()
-                                    game:start_dialog("final.zelda_8", function()
-                                      sol.timer.start(map, 500, function()
-                                        -- TODO draw heart shape instead of ellipse
-                                        fade_sprite = sol.sprite.create("entities/heart_fade")
-                                        local camera_x, camera_y = map:get_camera():get_position()
-                                        local zelda_x, zelda_y = grump_and_zelda:get_position()
-                                        fade_x = zelda_x - camera_x
-                                        fade_y = zelda_y - camera_y - 16
-                                        fade_sprite:set_animation("close", function()
-                                          -- Fill screen with black.
-                                          local quest_w, quest_h = sol.video.get_quest_size()
-                                          black_surface = sol.surface.create(quest_w, quest_h)
-                                          black_surface:fill_color({0, 0, 0})
+                                sol.timer.start(map, 2000, function()
+                                  local hero_movement_4 = sol.movement.create("target")
+                                  hero:set_animation("walking")
+                                  hero:set_direction(2) -- left
+                                  hero_movement_4:set_target(-32, 192)
+                                  hero_movement_4:set_speed(80)
+                                  hero_movement_4:set_smooth(true)
+                                  hero_movement_4:set_ignore_obstacles(true)
+                                  hero_movement_4:start(hero, function()
+                                    sol.timer.start(map, 1000, function()
+                                      game:start_dialog("final.zelda_8", function()
+                                        sol.timer.start(map, 500, function()
+                                          -- TODO draw heart shape instead of ellipse
+                                          fade_sprite = sol.sprite.create("entities/heart_fade")
+                                          local camera_x, camera_y = map:get_camera():get_position()
+                                          local zelda_x, zelda_y = grump_and_zelda:get_position()
+                                          fade_x = zelda_x - camera_x
+                                          fade_y = zelda_y - camera_y - 16
+                                          fade_sprite:set_animation("close", function()
+                                            -- Fill screen with black.
+                                            local quest_w, quest_h = sol.video.get_quest_size()
+                                            black_surface = sol.surface.create(quest_w, quest_h)
+                                            black_surface:fill_color({0, 0, 0})
 
-                                          sol.timer.start(map, 1000, function()
-                                            local menu_font, menu_font_size = language_manager:get_menu_font()
-                                            end_text = sol.text_surface.create{
-                                              horizontal_alignment = "center",
-                                              vertical_alignment = "middle",
-                                              color = {255, 255, 255},
-                                              font = menu_font,
-                                              font_size = menu_font_size * 2,
-                                              text_key = "final.end_text",
-                                            }
+                                            sol.timer.start(map, 1000, function()
+                                              local menu_font, menu_font_size = language_manager:get_menu_font()
+                                              end_text = sol.text_surface.create{
+                                                horizontal_alignment = "center",
+                                                vertical_alignment = "middle",
+                                                color = {255, 255, 255},
+                                                font = menu_font,
+                                                font_size = menu_font_size * 2,
+                                                text_key = "final.end_text",
+                                              }
 
-                                            sol.timer.start(map, 2000, function()
-                                              -- Hide text
-                                              end_text = nil
-                                              -- Launch Ending credits.
-                                              local dialog_box = game:get_dialog_box()
-                                              dialog_box:set_position("bottom")
-                                              game:start_dialog("final.credits", function()
-                                                -- Reset game.
-                                                sol.main.reset()
+                                              sol.timer.start(map, 2000, function()
+                                                -- Hide text
+                                                end_text = nil
+                                                -- Launch Ending credits.
+                                                local dialog_box = game:get_dialog_box()
+                                                dialog_box:set_position("bottom")
+                                                game:start_dialog("final.credits", function()
+                                                  -- Reset game.
+                                                  sol.main.reset()
+                                                end)
                                               end)
                                             end)
                                           end)
@@ -235,6 +236,5 @@ function map:start_cinematic()
         end)
       end)
     end)
-  end)
   end)
 end

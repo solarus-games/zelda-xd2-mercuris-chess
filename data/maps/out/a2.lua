b9bbf6339fad3c0ae1b7e9e6f88aab391913e435
Christopho <christopho@solarus-games.org>
2017-04-03 23:13:11 +0200

    Karts on bananas

--- data/maps/out/a2.lua
+++ data/maps/out/a2.lua
@@ -24,7 +24,12 @@ local track_path = {
   1,1, 1,1, 1,1, 1,1,
 }
 
-local function start_kart(kart, initial_path_index, speed, harmful)
+local function start_kart(kart, properties)
+
+  local initial_path_index = properties.initial_path_index
+  local speed = properties.speed or 96
+  local harmful = properties.harmful or false
+  local hurt_sound = properties.hurt_sound
 
   local path = {}
   for i = initial_path_index, #track_path do
@@ -39,19 +44,6 @@ local function start_kart(kart, initial_path_index, speed, harmful)
   kart:set_traversable_by("hero", true)
   kart:set_traversable_by("custom_entity", true)
 
-  if harmful then
-    -- Hurt the hero.
-    kart:add_collision_test("sprite", function(kart, other)
-      if other ~= hero then
-        return
-      end
-
-      if not hero:is_invincible() then
-        hero:start_hurt(kart, 6)
-      end
-    end)
-  end
-
   local sprite = kart:get_sprite()
   sprite:set_animation("walking")
 
@@ -64,7 +56,81 @@ local function start_kart(kart, initial_path_index, speed, harmful)
   movement:start(kart)
 
   function movement:on_changed()
-    sprite:set_direction(movement:get_direction4())
+    if not kart.banana_collision then
+      sprite:set_direction(movement:get_direction4())
+    end
+  end
+
+  if harmful then
+    -- Hurt the hero.
+    kart:add_collision_test("sprite", function(kart, other)
+      if other ~= hero then
+        return
+      end
+
+      if hero:is_invincible() then
+        return
+      end
+
+      if kart.banana_collision then
+        -- The kart was hurt.
+        return
+      end
+
+      hero:start_hurt(kart, 6)
+    end)
+
+    -- React to banana skins.
+    kart:add_collision_test("overlapping", function(kart, other)
+      if other:get_type() ~= "pickable" then
+        return
+      end
+
+      if other:get_treasure():get_name() ~= "banana_skin" then
+        return
+      end
+
+      if kart.banana_collision then
+        -- Already done.
+        return
+      end
+
+      kart.banana_collision = true
+      other:remove()
+
+      if hurt_sound ~= nil then
+        sol.audio.play_sound(hurt_sound)
+      end
+
+      sol.timer.start(kart, 100, function()
+        sprite:set_direction((sprite:get_direction() + 1) % 4)
+        -- Keep turning while the movement is active.
+        return kart:get_movement() ~= nil
+      end)
+
+      local hurt_movement = sol.movement.create("straight")
+      local direction4 = movement:get_direction4()
+      local random_angle = (math.random() * math.pi / 6) - math.pi / 12
+      local angle = (direction4 * math.pi / 2) + random_angle
+      hurt_movement:set_angle(angle)
+      hurt_movement:set_speed(speed)
+      hurt_movement:set_max_distance(192)
+      hurt_movement:set_smooth(true)
+      hurt_movement:start(kart, function()
+        hurt_movement:on_obstacle_reached()
+      end)
+
+      function hurt_movement:on_obstacle_reached()
+        kart:stop_movement()
+        sol.timer.start(kart, 50, function()
+          kart:set_visible(not kart:is_visible())
+          return true
+        end)
+        sol.timer.start(kart, 1000, function()
+          kart:remove()
+        end)
+      end
+    end)
   end
 
 end
@@ -74,10 +140,29 @@ function map:on_started()
   local movement = sol.movement.create("random_path")
   movement:start(doc)
 
-  start_kart(toad, 133, math.random(96, 128), true)
-  start_kart(mario, 7, math.random(64, 128), true)
-  start_kart(yoshi, 45, math.random(96, 192), true)
-  start_kart(deloreane, 95, math.random(64, 96), false)
+  start_kart(toad, {
+    initial_path_index = 133,
+    speed = math.random(96, 128),
+    harmful = true,
+    hurt_sound = "mk64_toad",
+  })
+  start_kart(mario, {
+    initial_path_index = 7,
+    speed = math.random(64, 128),
+    harmful = true,
+    hurt_sound = "mk64_mario_mammamia",
+  })
+  start_kart(yoshi, {
+    initial_path_index = 45,
+    speed = math.random(96, 192),
+    harmful = true,
+    hurt_sound = "mk64_yoshi_hurt",
+  })
+  start_kart(deloreane, {
+    initial_path_index = 95,
+    speed = math.random(64, 96),
+    hurt_sound = false,
+  })
 
   deloreane:get_sprite():set_animation("flying_no_shadow")
   deloreane:set_traversable_by(true)  -- Because flying.
